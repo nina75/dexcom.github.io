c933da9c6c603315d784fe51f186b7101e20d6f8
b0rizz <borizkoff@gmail.com>
2019-05-31 18:49:07 +0300

    fetch EGVS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,71 @@
 $(function () {
-    $('#datetimepicker1').datetimepicker();
+    
+    const uri = 'https://summertime-sadness.herokuapp.com/api/dexcom/egvs';
+    const formEgvs = $('#form-egvs');
+    const spinner = $('#spiner-html').html();
+    
+    $('#dp-start-date').datetimepicker({
+      locale: 'bg',
+      format: 'DD-MM-YYYY HH:mm:ss',
+      defaultDate: moment(new Date(), 'DD-MM-YYYY HH:mm:ss').subtract(7,'d'),
+    });
+    
+    $('#dp-end-date').datetimepicker({
+      locale: 'bg',
+      format: 'DD-MM-YYYY HH:mm:ss',
+      defaultDate: new Date(),
+    });
+    
+    
+    (function() {
+      
+      const startDate = moment( $('[name=startDate]', formEgvs).val(), 'DD-MM-YYYY HH:mm:ss', true ).format('YYYY-MM-DD\TH:m:s');
+      const endDate = moment( $('[name=endDate]', formEgvs).val(), 'DD-MM-YYYY HH:mm:ss', true ).format('YYYY-MM-DD\TH:m:s');
+      
+      getEgvs( uri, {startDate: startDate, endDate: endDate} );
+    })();
+    
+    
+    $('.js-btn-fetch-egvs', formEgvs).on('click', function (e) {
+      e.preventDefault();
+      
+      $('.js-average-egvs').html(spinner);
+      
+      const startDate = moment( $('[name=startDate]', formEgvs).val(), 'DD-MM-YYYY HH:mm:ss', true ).format('YYYY-MM-DD\TH:m:s');
+      const endDate = moment( $('[name=endDate]', formEgvs).val(), 'DD-MM-YYYY HH:mm:ss', true ).format('YYYY-MM-DD\TH:m:s');
+      
+      getEgvs( uri, {startDate: startDate, endDate: endDate} );
+
+    });
+    
+    
+    function getEgvs(url, data) {
+
+      $.ajax({
+        type: "GET",
+        url: url,
+        data: data,
+        cache: false,
+      }).done(function (response) {
+
+        if(response.success) {
+
+          const egvs = response.data.egvs;
+          let average = 0;
+
+          for(let i = 0; i < egvs.length; i++) {
+            
+            average += egvs[i]['value'];
+          }
+
+          $('.js-average-egvs').html( Math.round(average/egvs.length) );
+        }
+      }).fail(function (error) {
+        console.log('------error-----')
+        console.log(error);
+        console.log('------end error------');
+      });
+    }
 });
 
 //~ google.charts.load('current', {'packages':['corechart']});
@@ -23,3 +89,4 @@ $(function () {
         //~ var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
         //~ chart.draw(data, options);
       //~ }
+      
